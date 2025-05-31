26afd144d6033b159938ad3cdc9ebe1e0e8a1c2c
kyw <107684857+kyw-kyw@users.noreply.github.com>
2025-05-31 21:35:41 +0900

    Add: Attributes

---
+++ src/UIToolkitBinding/Accessibility.cs
@@ -0,0 +1,43 @@
+﻿namespace UIToolkitBinding;
+
+#if SOURCE_GENERATOR
+internal
+#else
+public
+#endif
+enum DeclaredAccessibility : byte
+{
+    /// <summary>access modifier is <see langword="public"/></summary>
+    Public = 0,
+    /// <summary>access modifier is <see langword="protected"/></summary>
+    Protected = 1,
+    /// <summary>access modifier is <see langword="internal"/></summary>
+    Internal = 2,
+    /// <summary>access modifier is <see langword="protected"/> <see langword="internal"/></summary>
+    ProtectedInternal = 3,
+    /// <summary>access modifier is <see langword="private"/></summary>
+    Private = 4,
+    /// <summary>access modifier is <see langword="private"/> <see langword="protected"/></summary>
+    PrivateProtected = 5
+}
+
+#if SOURCE_GENERATOR
+internal
+#else
+public
+#endif
+enum SetterAccessibility : byte
+{
+    /// <summary>access modifier is <see langword="public"/></summary>
+    Public = 0,
+    /// <summary>access modifier is <see langword="protected"/></summary>
+    Protected = 1,
+    /// <summary>access modifier is <see langword="internal"/></summary>
+    Internal = 2,
+    /// <summary>access modifier is <see langword="protected"/> <see langword="internal"/></summary>
+    ProtectedInternal = 3,
+    /// <summary>access modifier is <see langword="private"/></summary>
+    Private = 4,
+    /// <summary>access modifier is <see langword="private"/> <see langword="protected"/></summary>
+    PrivateProtected = 5
+}
---
+++ src/UIToolkitBinding/Attributes.cs
@@ -0,0 +1,27 @@
+﻿namespace UIToolkitBinding;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+public sealed class UITKDataSourceObjectAttribute : Attribute;
+
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+public sealed class UITKBindableFieldAttribute : Attribute
+{
+    public UITKBindableFieldAttribute()
+        : this(DeclaredAccessibility.Public, SetterAccessibility.Public)
+    {
+    }
+
+    public UITKBindableFieldAttribute(SetterAccessibility setterAccessibility)
+        : this(DeclaredAccessibility.Public, setterAccessibility)
+    {
+    }
+
+    public UITKBindableFieldAttribute(DeclaredAccessibility declaredAccessibility, SetterAccessibility setterAccessibility)
+    {
+        DeclaredAccessibility = declaredAccessibility;
+        SetterAccessibility = setterAccessibility;
+    }
+
+    public DeclaredAccessibility DeclaredAccessibility { get; }
+    public SetterAccessibility SetterAccessibility { get; }
+}
