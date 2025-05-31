167a16c9ea37886dda8de791df75ec9674158822
kyw <107684857+kyw-kyw@users.noreply.github.com>
2025-05-31 23:11:34 +0900

    Add: UIToolkitBinding.SourceGenerator.csproj

---
+++ src/UIToolkitBinding.SourceGenerator/UIToolkitBindingSourceGenerator.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace UIToolkitBinding;
+
+[Generator(LanguageNames.CSharp)]
+public sealed class UIToolkitBindingSourceGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+    }
+}
