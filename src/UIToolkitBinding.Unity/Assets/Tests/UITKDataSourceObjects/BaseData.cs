f8e44e7a66c656b224dcfb422a209ef125254ed1
kyw <107684857+kyw-kyw@users.noreply.github.com>
2025-06-07 18:04:44 +0900

    Add: UIToolkitBinding.Unity.Tests

---
+++ src/UIToolkitBinding.Unity/Assets/Tests/UITKDataSourceObjects/BaseData.cs
@@ -0,0 +1,16 @@
+﻿namespace UIToolkitBinding.Unity.Tests
+{
+    [UITKDataSourceObject]
+    internal partial class Base
+    {
+        [UITKBindableField]
+        int baseData;
+    }
+
+    [UITKDataSourceObject]
+    internal partial class Sub : Base
+    {
+        [UITKBindableField]
+        int subData;
+    }
+}
---
+++ src/UIToolkitBinding.Unity/Assets/Tests/UITKDataSourceObjects/User.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.UIElements;
+
+namespace UIToolkitBinding.Unity.Tests
+{
+    internal class User
+    {
+        public string Name { get; set; }
+    }
+
+    [UITKDataSourceObject]
+    internal partial class BindableUser
+    {
+        internal readonly BindablePropertyChangedEventArgs NameChanged = new("Name");
+        public BindableUser(User user)
+        {
+            UserProxy = user;
+        }
+
+        public User UserProxy { get; }
+
+        public string Name
+        {
+            get => UserProxy.Name;
+            set => SetProperty(UserProxy.Name, value, UserProxy, (user, name) => user.Name = name, NameChanged);
+        }
+    }
+
+    [UITKDataSourceObject]
+    internal partial class UITKUser
+    {
+        [UITKBindableField]
+        string name;
+    }
+}
