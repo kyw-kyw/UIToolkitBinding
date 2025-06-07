85a45ab0c2f19e6e00136a6d930707ee7bee984c
kyw <107684857+kyw-kyw@users.noreply.github.com>
2025-06-07 20:10:13 +0900

    Add: Rotation Sample

---
+++ src/UIToolkitBinding.Unity/Assets/Sample/Scripts/Rotation.cs
@@ -0,0 +1,44 @@
+﻿using UIToolkitBinding;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Sample
+{
+    internal partial class Rotation : MonoBehaviour
+    {
+        [SerializeField] Transform rotationTarget;
+        [SerializeField] UIDocument document;
+        VisualElement root;
+
+        void Awake()
+        {
+            root = document.rootVisualElement.Q<VisualElement>("roration__main-container");
+        }
+
+        void OnEnable()
+        {
+            var dataSource = new EulerAngleData(rotationTarget);
+            root.dataSource = dataSource;
+        }
+
+        [UITKDataSourceObject]
+        public partial class EulerAngleData
+        {
+            Transform target;
+
+            [UITKBindableField(SetterAccessibility.Private)]
+            Vector3 eulerAngles;
+
+            public EulerAngleData(Transform transform)
+            {
+                target = transform;
+                eulerAngles = transform.eulerAngles;
+            }
+
+            partial void OnEulerAnglesChanged(Vector3 newValue)
+            {
+                target.eulerAngles = newValue;
+            }
+        }
+    }
+}
